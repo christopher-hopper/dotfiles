170101f7190901855bf7d4d4ff1f278e80e71a64
Christopher Hopper <christopher.jf.hopper@gmail.com>
2014-08-21 16:14:49 +1000

    Move vless alias from profile.d to bash_alias

--- profile.d/vless.sh
+++
@@ -1,4 +0,0 @@
-if [ -n "$BASH_VERSION" -o -n "$KSH_VERSION" -o -n "$ZSH_VERSION" ]; then
-  [ -x /usr/share/vim/vimfiles/macros/less.sh ] || return
-  alias vless >/dev/null 2>&1 || alias vless='/usr/share/vim/vimfiles/macros/less.sh'
-fi
