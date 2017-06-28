508354e6506c1d613db1856f49443703f89c6269
Christopher Hopper <chopper@deloitte.com.au>
2017-06-28 10:43:30 +1000

    OSX freebsd install fixes

--- install.sh
+++ install.sh
@@ -1,8 +1,44 @@
 #!/bin/bash
 # vim: ft=sh ts=4 sw=3
+
+# install readlink
+#
+# OSX freeBSD friendly `readlink -f` alternative.
+#
+# @return string
+#   Canonical path to this shell script.
+function install_dotreal
+{
+   local ORIGPWD=$(pwd);
+   local TARGET=$(basename "$0");
+   cd $(dirname "$0");
+
+   # Iterate down a (possible) chain of symlinks
+   while [ -L "$TARGET" ]
+   do
+	  TARGET=$(readlink "$TARGET");
+	  cd $(dirname "$TARGET");
+	  TARGET=$(basename "$TARGET");
+   done
+
+   # Compute the canonicalized directory by printing the physical working dir.
+   local CDIR=$(pwd -P);
+
+   # Return to the original working dir.
+   cd "$ORIGPWD";
+
+   echo "$CDIR/$TARGET";
+}
+
+# install dotfiles
+#
+# Install the dotfiles into the current user home directory.
+#
+# @return string
+#   Logging of installation steps and result.
 function install_dotfiles
 {
-DOTREAL="$(readlink -f "${0}")";
+DOTREAL="$(install_dotreal)";
 DOTPATH="${DOTREAL%/*}";
 DOTBAKX="orig~";
 BAKLIST=();
@@ -44,5 +80,5 @@ fi;
 }
 
 install_dotfiles;
-unset install;
+unset install_dotreal install_dotfiles;
 exit 0;
